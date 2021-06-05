0d669a2e02677e8953482b8e82068aca462010b4
maath leo <maathleo01@gmail.com>
2021-06-05 15:23:11 +0530

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	new file:   project/000_admin.json 	new file:   project/api/__init__.py 	new file:   project/api/__pycache__/__init__.cpython-38.pyc 	new file:   project/api/__pycache__/admin.cpython-38.pyc 	new file:   project/api/__pycache__/apps.cpython-38.pyc 	new file:   project/api/__pycache__/models.cpython-38.pyc 	new file:   project/api/admin.py 	new file:   project/api/apps.py 	new file:   project/api/migrations/0001_initial.py 	new file:   project/api/migrations/0002_auto_20210605_0842.py 	new file:   project/api/migrations/0003_auto_20210605_0851.py 	new file:   project/api/migrations/0004_auto_20210605_0933.py 	new file:   project/api/migrations/__init__.py 	new file:   project/api/migrations/__pycache__/0001_initial.cpython-38.pyc 	new file:   project/api/migrations/__pycache__/0002_auto_20210605_0842.cpython-38.pyc 	new file:   project/api/migrations/__pycache__/0003_auto_20210605_0851.cpython-38.pyc 	new file:   project/api/migrations/__pycache__/0004_auto_20210605_0933.cpython-38.pyc 	new file:   project/api/migrations/__pycache__/__init__.cpython-38.pyc 	new file:   project/api/models.py 	new file:   project/api/tests.py 	new file:   project/api/views.py 	new file:   project/author.json 	new file:   project/db.sqlite3 	new file:   project/project/__pycache__/__init__.cpython-38.pyc 	new file:   project/project/__pycache__/settings.cpython-38.pyc 	new file:   project/project/__pycache__/urls.cpython-38.pyc 	new file:   project/project/__pycache__/wsgi.cpython-38.pyc 	modified:   project/project/settings.py

---
+++ project/api/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.db.models import CharField, IntegerField, TextField
+from django.core.validators import validate_email
+from django.core.validators import RegexValidator
+
+import re
+
+# Create your models here.
+
+def validate_password(key):
+    try:
+        if len(key)>7:
+            if bool(re.findall('[A-Z]+', key)) and bool(re.findall('[a-z]+', key)):
+                return key
+            else:
+                raise ValidationError('number format not acceptable. validlength accuracy: 10')
+    except:
+        raise ValueError
+
+class Author(models.Model):
+    email = models.EmailField(primary_key=True, max_length=254, blank=False, unique=True, validators=[validate_email])
+    password = models.CharField(max_length=254, blank=False, unique=False, validators=[validate_password])
+    fullName = CharField(max_length=254, blank=False, unique=False)
+    phone = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{10,10}$')])
+    address = models.TextField(max_length=254, blank=True)
+    city = models.CharField(max_length=254, blank=True)
+    state = models.CharField(max_length=254, blank=True)
+    country = models.CharField(max_length=254, blank=True)
+    pincode = models.CharField(max_length=6, validators=[RegexValidator(r'^\d{1,6}$')])
+
+class Contitem(models.Model):
+    title = models.CharField(max_length=30, blank=False)
+    body = models.TextField(max_length=300, blank=False)
+    summary = models.TextField(max_length=60, blank=False)
+
+class Admin(models.Model):
+    username = models.CharField(primary_key=True, max_length=254, blank=False, unique=True)
+    password = models.CharField(max_length=254, blank=False, validators=[validate_email])
